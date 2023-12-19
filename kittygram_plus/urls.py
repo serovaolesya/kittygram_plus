1521131ffc0a5c90137fc82f184c9c720e6caed2
Олеся Серова <olesia-serova@yandex.ru>
2023-12-19 20:04:14 +0300

    Another commit.

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -4,12 +4,13 @@ from rest_framework.routers import DefaultRouter
 
 from django.urls import include, path
 
-from cats.views import CatViewSet, OwnerViewSet
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
 router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
